2ed5708c0f9170b98bdfa6b943c0a6ab9d08642c
seunghyuns98 <55797829+seunghyuns98@users.noreply.github.com>
2024-03-27 06:35:02 +0900

    Update basis.py

--- models/basis.py
+++ models/basis.py
@@ -4,9 +4,10 @@ import torch.nn.functional as F
 
 
 def conv1x1(in_planes, out_planes, stride=1):
-    """1x1 convolution"""
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
-
+    
+def conv3x3(in_planes, out_planes, stride=1):
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=False)
 
 class ResBlk(nn.Module):
     def __init__(self, inplanes, planes, stride=1, downsample=None):
